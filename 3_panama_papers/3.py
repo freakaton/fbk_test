f8a6596b6c6ba4c1389653cdbd14a87f64a74943
Ivan Lavrenov <dev5@topnomer.ru>
2019-06-22 23:39:56 +0300

    improve 3

--- 3_panama_papers/3.py
+++ 3_panama_papers/3.py
@@ -1,16 +1,6 @@
 import csv
 
 
-def check_for_company(file, companies):
-    checks = []
-    db = csv.DictReader(file)
-    for row in db:
-
-        if row['name'].strip().lower() in companies.keys():
-            checks.append(row)
-    return checks
-
-
 def get_all_companies(file):
     gb_db = csv.DictReader(file)
     companies = {}
@@ -18,14 +8,58 @@ def get_all_companies(file):
     for row in gb_db:
         name = row['Proprietor Name (1)']
         if name:
-            companies[name.strip().lower()] = row
+            company = dict()
+            company['Title_Number'] = row['Title Number']
+            company['Property_Address'] = row['Property Address']
+            companies[row['Proprietor Name (1)'].strip().lower()] = company
     return companies
 
 
-if __name__ == '__main__':
+def check_for_company(file, companies):
+    checks = []
+    db = csv.DictReader(file)
+    for row in db:
+        name = row['name'].strip().lower()
+        check = companies.get(name, None)
+        if check:
+            check['Node_Id'] = row['node_id']
+            check['Company_Name'] = row['name']
+            check['Owner'] = ''
+            checks.append(check)
+    return checks
+
+
+def find_owner_ids(file, companies):
+    db = csv.DictReader(file)
+    owner_statuses = {'shareholder', 'owner'}
+    intermediary = 'intermediary of'
+    ids = {cp['Node_Id'] for cp in companies}
+    resp = dict()
+    for id in ids:
+        resp[id] = []
+
+    for row in db:
+        if row["END_ID"] in ids:
+            if row['link'] in owner_statuses:
+                resp[row["END_ID"]] = row['START_ID']
+            elif row['link'] == intermediary:
+                pass
+            # TODO: Доделать
+    return resp
+
+def main():
     with open('src/OCOD_FULL_2019_06.csv', 'r') as f:
         companies = get_all_companies(f)
     with open('src/Panama_papers/panama_papers.nodes.entity.csv', 'r') as f:
-        checks = check_for_company(f, companies)
-        import pdb
-        pdb.set_trace()
+        companies = check_for_company(f, companies)
+    with open('src/Panama_papers/panama_papers.edges.csv') as f:
+        owner_ids = find_owner_ids(f, companies)
+    with open('result.csv', 'w') as f:
+        fieldnames = ['Title_Number', 'Property_Address', 'Company_Name', 'Owner', 'Node_Id']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(companies)
+
+
+if __name__ == '__main__':
+    main()
